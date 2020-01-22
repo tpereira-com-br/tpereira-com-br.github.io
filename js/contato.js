242085fc76f347c5ee1ecaa0d8cc8c6ea85bc2f8
tpereira-com-br <contato.tpereira.com.br>
2020-01-22 02:00:57 -0300

    Correction

--- js/contato.js
+++ js/contato.js
@@ -1,1 +1,1 @@
-function contato(e,n,t,a){var o=new FormData;o.append("nome",e),o.append("email",n),o.append("telefone",t),o.append("mensagem",a),fetch("http://www.tpereira.com.br/php/Email/contato",{method:"GET",body:o}).then((function(e){return e.json()})).then((function(e){e.alert&&alert(e.alert)}))}
+function contato(e,n,t,a){var o=new FormData;o.append("nome",e),o.append("email",n),o.append("telefone",t),o.append("mensagem",a),fetch("http://www.tpereira.com.br/php/Email/contato",{method:"POST",body:o}).then((function(e){return e.json()})).then((function(e){e.alert&&alert(e.alert)}))}
