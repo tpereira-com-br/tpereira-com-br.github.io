b2a4c3861c3ab7afe9af673b813d8250a3cea8f0
tpereira-com-br <contato.tpereira.com.br>
2020-01-22 01:58:21 -0300

    bug resolve

--- js/contato.js
+++ js/contato.js
@@ -1,18 +1,1 @@
-$(document).ready(function(){
-  $("#enviar").click(function(){
-    var nome = $("#nome").val();
-    var email = $("#email").val();
-    var telefone = $("#telefone").val();
-    var mensagem = $("#mensagem").val();
-    $.post("../php/Email/contato",
-    {
-      nome:nome,
-      email:email,
-      telefone:telefone,
-      mensagem:mensagem
-    },
-    function(data){
-      alert(data);
-    });
-  });
-});
+function contato(e,n,t,a){var o=new FormData;o.append("nome",e),o.append("email",n),o.append("telefone",t),o.append("mensagem",a),fetch("http://www.tpereira.com.br/php/Email/contato",{method:"GET",body:o}).then((function(e){return e.json()})).then((function(e){e.alert&&alert(e.alert)}))}
